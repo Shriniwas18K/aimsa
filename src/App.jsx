920aa6bc6b533e4162a4c81f007cc1a346e046cc
Shriniwas18K <kshriniwas180205@gmail.com>
2023-12-25 00:39:46 +0530

    optimized loading time

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React,{ Suspense, useState } from 'react';
 import Home from './pages/home/Home';
-import Team from './pages/team/Team';
-import Events from './pages/events/Events';
+const Team = React.lazy(() => import('./pages/team/Team'));
+const Events=React.lazy(()=>import('./pages/events/Events'));
 import Footer from './Footer';
 import './App.css';
 export default function App() {
@@ -39,9 +39,12 @@ export default function App() {
         </button>
       </nav>
       <section id="main">
-        {currentPage === 'HOME' ? <Home /> :
-          currentPage === 'TEAM' ? <Team /> :
-            currentPage === 'EVENTS' ? <Events /> : null
+        {currentPage === 'HOME' ?
+         <Suspense fallback={<section>loading</section>}><Home /></Suspense>:
+          currentPage === 'TEAM' ? 
+          <Suspense fallback={<section>loading</section>}><Team /></Suspense> :
+            currentPage === 'EVENTS' ? 
+            <Suspense fallback={<section>loading</section>}><Events /></Suspense>: null
         }
       </section>
       <Footer />
